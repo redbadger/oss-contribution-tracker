6bcb7e04e29e844d01819fa3400f7a15bb44aba2
Viktor Charypar <charypar@gmail.com>
2016-05-06 17:55:38 +0100

    More contribution processing

--- scraper/src/scraper/core.clj
+++ scraper/src/scraper/core.clj
@@ -4,12 +4,37 @@
 
 (def gh (github/request http/get))
 
+(defn process-user
+  [user]
+  (let [direct ((github/user-public-repos gh) user)
+        orgs ((github/user-orgs gh) user)
+        issues ((github/user-issues gh) user)]
+    (concat direct orgs issues)))
+
+(defn pass-through
+  "returns a transform that applies a mapping-fn to a key of a map if the map has that key, otherwise
+  leaves it alone"
+  [map-key mapping-fn]
+  (fn [item]
+    (if (contains? item map-key)
+      (mapping-fn item)
+      [item])))
+
+(defn split-issue
+  [{:keys [user issue repo]}]
+  [{:user user :issue issue}
+   {:user user :repo repo}])
+
 (def tx-stack
   (comp
     (mapcat (github/org-members gh))
     (filter identity)
-    (mapcat (github/user-public-repos gh))))
+    (take 2)
+    (mapcat process-user)
+    (mapcat (pass-through :org (github/org-public-repos gh)))
+    (mapcat (pass-through :issue split-issue))
+    (mapcat (pass-through :repo (github/repo-commits gh)))))
 
 (defn -main []
-  (doseq [repo (sequence tx-stack ["redbadger"])]
-    (println "repo:" repo)))
+  (doseq [contribs (sequence tx-stack [{:org "redbadger"}])]
+    (clojure.pprint/pprint contribs)))
